34c48df4d1babf954579f655aea26005e42fb39a
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2020-06-01 16:51:43 -0300

    mini projeto de QRcode , gera uma senha aleatoria com 8 caracter e depois gera o QRcode dela

---
+++ QrCode/qrcode.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import random
+import pyqrcode
+
+
+#lista de letras para gerar um senha aleatoria
+letras = ['a','b','c','d','e',
+          'f','g','h','i','j',
+          'k','l','m','n','o',
+          'p','q','r','s','t',
+          'u','v','x','y','z']
+
+#senha gerada 
+senha = []
+
+#conversao para string
+ts = ''
+
+# Gera uma senha com 8 caracter e salva em senha
+for senha_num in range(0,9):
+    senha_formada = random.choice(letras)
+    senha.append(senha_formada)
+
+# converte em string e guarda em ts
+for i in senha:
+    ts += i
+print(senha)
+print(ts)
+
+
+
+# pega a string  ts
+qr = ts
+# Saida nome do arquivo
+saida = input('teste')
+
+# Cria o qrcode
+url = pyqrcode.create(qr)
+
+# Tipo do arquivo e tamanho
+url.svg(saida +'.odt',scale = 8)
+
+
+print(url)
+
+
+
+
